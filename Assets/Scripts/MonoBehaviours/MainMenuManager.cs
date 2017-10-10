ffc146273dbfcec86f0ff56639b6a4602045ca8c
Akomer <spdrhu@gmail.com>
2017-10-10 13:10:35 +0200

    Add quit button to main menu

--- Assets/Scripts/MonoBehaviours/MainMenuManager.cs
+++ Assets/Scripts/MonoBehaviours/MainMenuManager.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour {
+public class MainMenuManager : MonoBehaviour
+{
 
     public void StartOrdering()
     {
         SceneManager.LoadScene("Ordering");
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
     }
 }
